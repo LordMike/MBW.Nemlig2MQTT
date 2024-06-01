2ca6544803c44a618d2b4846fc2f4b5e4332c90a
Michael Bisbjerg <michael@mbwarez.dk>
2024-06-01 19:37:39 +0200

    Fix nullable

--- MBW.Client.NemligCom/Objects/Order/LatestOrderHistoryOrder.cs
+++ MBW.Client.NemligCom/Objects/Order/LatestOrderHistoryOrder.cs
@@ -6,7 +6,7 @@ namespace MBW.Client.NemligCom.Objects.Order;
 
 public class LatestOrderHistoryOrder : Order
 {
-    [JsonConverter(typeof(TimezoneDateTimeConverter), "Europe/Copenhagen")]
+    [JsonConverter(typeof(OptionalTimezoneDateTimeConverter), "Europe/Copenhagen")]
     public DateTimeOffset? EstimatedArrivalTime { get; set; }
     // public object ActualArrivalTime { get; set; }
     // public object Buttons { get; set; }
