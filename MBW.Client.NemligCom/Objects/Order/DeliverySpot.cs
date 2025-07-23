bc820d2b6f3d0c06a2b8f18e7816188663a4967a
Michael Bisbjerg <michael@mbwarez.dk>
2025-07-23 12:33:37 +0200

    Add GetDeliverySpot API method

---
+++ MBW.Client.NemligCom/Objects/Order/DeliverySpot.cs
@@ -0,0 +1,14 @@
+namespace MBW.Client.NemligCom.Objects.Order;
+
+public class DeliverySpot
+{
+    public string Id { get; set; }
+    public string CustomerName { get; set; }
+    public string OrderNumber { get; set; }
+    public DeliverySpotState State { get; set; }
+    public Deliverytime TimeSlot { get; set; }
+    public DateTime EditDeadline { get; set; }
+    public float Progress { get; set; }
+    public DateTime DeliveryTime { get; set; }
+    public Deliverytime DeliveryInterval { get; set; }
+}
---
+++ MBW.Client.NemligCom/Objects/Order/DeliverySpotState.cs
@@ -0,0 +1,8 @@
+namespace MBW.Client.NemligCom.Objects.Order;
+
+public enum DeliverySpotState
+{
+    None = 0,
+    OngoingDelivery,
+    CompletedDelivery
+}
